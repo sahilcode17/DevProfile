91bb5830046f1835d0a34da0181e09ad0103359c
Sahil khandelwal <khandelwalsahil09@gmail.com>
2021-09-07 11:42:25 +0530

    icon changed + minior changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,9 @@ if (currentTheme == "dark") {
   document.body.classList.toggle("light-theme");
 }
 
+
 btn.addEventListener("click", function () {
+ 
   if (prefersDarkScheme.matches) {
     document.body.classList.toggle("light-theme");
     var theme = document.body.classList.contains("light-theme")
